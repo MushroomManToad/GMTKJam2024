62d93dcfb64367f4ead9d38228bed60c1022c00b
MushroomManToad <mushroommantoad@hotmail.com>
2024-08-16 13:09:02 -0700

    Stage Loading Start

--- gmtk2024/scripts/game_manager.gd
+++ gmtk2024/scripts/game_manager.gd
@@ -6,3 +6,9 @@ func _ready():
 	# TODO Functions to be run to setup game after all scenes have been loaded.
 	print("Hello World")
 	pass
+
+
+func load_scene(pos: Vector2, scene_id: String):
+	var stage = load("res://scenes/stages/" + scene_id + ".tscn")
+	var stage_instance : Node2D = stage.instantiate()
+	stage_instance.global_position = pos
