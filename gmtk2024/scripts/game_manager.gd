a1be862c07c661d8bc03a07f0d3a231bbfbe983b
MushroomManToad <mushroommantoad@hotmail.com>
2024-08-16 13:38:09 -0700

    Camera Fixes
    Plus WIP Default Scene

--- gmtk2024/scripts/game_manager.gd
+++ gmtk2024/scripts/game_manager.gd
@@ -2,13 +2,18 @@ class_name GameManager
 
 extends Node
 
+@onready var game = $"/root/Game/"
+
 func _ready():
 	# TODO Functions to be run to setup game after all scenes have been loaded.
-	print("Hello World")
+	load_scene(Vector2(0,48), "dev_stage")
 	pass
 
 
 func load_scene(pos: Vector2, scene_id: String):
+	print("Loading stage ", scene_id)
 	var stage = load("res://scenes/stages/" + scene_id + ".tscn")
 	var stage_instance : Node2D = stage.instantiate()
 	stage_instance.global_position = pos
+	game.add_child(stage_instance)
+	print(stage, " : ", stage_instance, " : ")
