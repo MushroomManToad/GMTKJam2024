968ac2a0caa793fd5c12c097980aac8cf8b5264d
MushroomManToad <mushroommantoad@hotmail.com>
2024-08-17 13:38:00 -0700

    Field Update

--- gmtk2024/scripts/assets/field.gd
+++ gmtk2024/scripts/assets/field.gd
@@ -10,8 +10,29 @@ extends Area2D
 @onready var stretch_sprite: AnimatedSprite2D = $StretchSprite
 @onready var rotate_sprite: AnimatedSprite2D = $RotateSprite
 
+var colliding_bodies : Array
+
 func _ready():
 	# Set active sprite(s) by field
 	grow_sprite.visible = type_grow
 	stretch_sprite.visible = type_stretch
 	rotate_sprite.visible = type_rotate
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is IFieldInteractable:
+		(body as IFieldInteractable).set_in_field(true)
+		colliding_bodies.append(body)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is IFieldInteractable:
+		(body as IFieldInteractable).set_in_field(false)
+		colliding_bodies.erase(body)
+
+
+func dump_bodies() -> void:
+	for body in colliding_bodies:
+		if body is IFieldInteractable:
+			(body as IFieldInteractable).set_in_field(false)
+	colliding_bodies.clear()
--- gmtk2024/scripts/i_field_interactable.gd
+++ gmtk2024/scripts/i_field_interactable.gd
@@ -2,5 +2,21 @@ class_name IFieldInteractable
 
 extends Node2D
 
+var is_in_field : bool = false
+
 func _physics_process(delta: float) -> void:
+	if is_in_field:
+		_in_field_loop(delta)
+	else:
+		_out_field_loop(delta)
+
+# Loop when in a field
+func _in_field_loop(delta : float) -> void:
 	pass
+
+# Loop when out of a field
+func _out_field_loop(delta : float) -> void:
+	pass
+
+func set_in_field(val : bool) -> void:
+	is_in_field = val
