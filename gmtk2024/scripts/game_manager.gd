f9841a37f52750c4791474faad164d14ad4d34f5
MushroomManToad <mushroommantoad@hotmail.com>
2024-08-17 11:17:56 -0700

    Initial Level Load Adjustements

--- gmtk2024/scripts/game_manager.gd
+++ gmtk2024/scripts/game_manager.gd
@@ -9,14 +9,13 @@ var background : ParallaxBackground
 const player_asset = preload("res://scenes/player/player.tscn")
 const background_asset = preload("res://scenes/global/background.tscn")
 
-func _ready():
-	# TODO Functions to be run to setup game after all scenes have been loaded.
-	load_background(Vector2(0, 0))
-	load_scene(Vector2(0,48), "kristen_test_scene")
-	load_player(Vector2(0,0))
-	pass
+# Current array of loaded floor objects
+var loaded_floors : Array = [null, null, null]
 
-# Generic Stage Loading Function
+func _ready():
+	load_first_stage(Vector2(0, 0), "kristen_test_scene")
+
+# Generic Stage Loading Function. Called by relevant functions "first and "new
 func load_scene(pos: Vector2, scene_id: String):
 	var stage = load("res://scenes/stages/" + scene_id + ".tscn")
 	var stage_instance : Node2D = stage.instantiate()
@@ -33,3 +32,27 @@ func load_background(pos: Vector2) -> void:
 	var bg_instance : ParallaxBackground = background_asset.instantiate()
 	game.add_child(bg_instance)
 	background = bg_instance
+
+func load_new_scene(pos: Vector2, scene_id: String):
+	pass
+
+# Special variable setting for loading the tower's first floor
+func load_first_stage(pos: Vector2, scene_id: String):
+	load_background(Vector2(0, 0))
+	load_scene(pos, scene_id)
+	load_player(Vector2(0,0))
+	# Array setup
+	pass
+
+# Internal class for storing loaded floor data
+class Floor:
+	var scene_id : String
+	var pos : Vector2
+	
+	func set_vals(pos: Vector2, scene_id: String) -> void:
+		self.scene_id = scene_id
+		self.pos = pos
+	
+	# Returns an array of [Pos, Scene_ID]
+	func get_vals() -> Array:
+		return [pos, scene_id]
