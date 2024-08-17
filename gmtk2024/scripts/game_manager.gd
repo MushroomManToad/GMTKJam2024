8d7d5b092271a738a569973edf72a0a385e6a1fe
MushroomManToad <mushroommantoad@hotmail.com>
2024-08-17 09:38:17 -0700

    Parallax Background :)

--- gmtk2024/scripts/game_manager.gd
+++ gmtk2024/scripts/game_manager.gd
@@ -4,11 +4,14 @@ extends Node
 
 @onready var game = $"/root/Game/"
 var player : Player
+var background : ParallaxBackground
 
 const player_asset = preload("res://scenes/player/player.tscn")
+const background_asset = preload("res://scenes/global/background.tscn")
 
 func _ready():
 	# TODO Functions to be run to setup game after all scenes have been loaded.
+	load_background(Vector2(0, 0))
 	load_scene(Vector2(0,48), "kristen_test_scene")
 	load_player(Vector2(0,0))
 	pass
@@ -25,3 +28,8 @@ func load_player(pos: Vector2) -> void:
 	player_instance.global_position = pos
 	game.add_child(player_instance)
 	player = player_instance
+
+func load_background(pos: Vector2) -> void:
+	var bg_instance : ParallaxBackground = background_asset.instantiate()
+	game.add_child(bg_instance)
+	background = bg_instance
