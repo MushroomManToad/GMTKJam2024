d66bf0a0aaedc2db99887a05293f328bccc015f1
MushroomManToad <mushroommantoad@hotmail.com>
2024-08-18 16:50:32 -0700

    One Way Stretch

---
+++ gmtk2024/scripts/assets/one_way_tile.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var one_way: NinePatchRect = $OneWay
+@onready var collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
+
+func _ready() -> void:
+	var line_ = SegmentShape2D.new()
+	line_.a = Vector2(one_way.position[0], -9.0)
+	line_.b = Vector2(one_way.position[0] + one_way.size[0], -9.0)
+	collision_shape_2d.shape = line_
