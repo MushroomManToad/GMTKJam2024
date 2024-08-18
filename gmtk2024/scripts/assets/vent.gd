cc1418f889bf0937e1c0271cd2aa87f3e3fb0088
MushroomManToad <mushroommantoad@hotmail.com>
2024-08-17 23:09:11 -0700

    Imposter Sus

--- gmtk2024/scripts/assets/vent.gd
+++ gmtk2024/scripts/assets/vent.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+@export var height : float
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Resize the vent blowy range
+	collision_shape_2d.position[1] = -(height * 16.0) / 2.0
+	
+	var rect = RectangleShape2D.new()
+	rect.extents = Vector2(5.0, height * 8.0)
+	collision_shape_2d.shape = rect
