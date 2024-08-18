3e3dd7f8702e5b139059c09d62ab0ab1967372ef
MushroomManToad <mushroommantoad@hotmail.com>
2024-08-18 09:53:38 -0700

    Key visual improvements

--- gmtk2024/scripts/assets/key.gd
+++ gmtk2024/scripts/assets/key.gd
@@ -2,20 +2,26 @@ extends Area2D
 
 @export var door : KeyDoor
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var cpu_particles_2d: CPUParticles2D = $CPUParticles2D
 
-var to_door_vector : Vector2
+var to_door_vector : Vector2 = Vector2(0, 0)
 var collecting : bool = false
 var collecting_timer : float = 0.0
 
 func _ready():
-	to_door_vector = door.global_position - self.global_position
+	pass
 
 func _process(delta: float) -> void:
 	if collecting:
+		if not to_door_vector.length_squared() > 0:
+			to_door_vector = door.door_center_sprite.global_position - self.global_position
 		self.global_position += to_door_vector * delta
 		collecting_timer += delta
 		if collecting_timer >= 1.0:
 			open_door()
+		if collecting_timer >= 5.0:
+			self.queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -28,4 +34,5 @@ func collect_key():
 func open_door():
 	if door.is_closed:
 		door._on_toggle()
-	self.queue_free()
+	sprite_2d.visible = false
+	cpu_particles_2d.emitting = false
