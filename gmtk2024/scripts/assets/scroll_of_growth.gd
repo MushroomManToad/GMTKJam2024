dbb7385ee01f5efb2e62bc01298dba7b82a7e287
MushroomManToad <mushroommantoad@hotmail.com>
2024-08-19 19:26:41 -0700

    Scroll Update

--- gmtk2024/scripts/assets/scroll_of_growth.gd
+++ gmtk2024/scripts/assets/scroll_of_growth.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animation_player_2: AnimationPlayer = $AnimationPlayer2
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		collision_shape_2d.set_deferred("disabled", true)
 		body.spells_handler.unlock_spell("grow")
 		# Play animation that ends in queue_free
+		animation_player_2.play("Collect")
--- gmtk2024/scripts/assets/scroll_of_rotate.gd
+++ gmtk2024/scripts/assets/scroll_of_rotate.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animation_player_2: AnimationPlayer = $AnimationPlayer2
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -9,3 +10,4 @@ func _on_body_entered(body: Node2D) -> void:
 		body.spells_handler.unlock_spell("stretch")
 		body.spells_handler.unlock_spell("rotate")
 		# Play animation that ends in queue_free
+		animation_player_2.play("Collect")
