554b1788d4dc7530c0701b2992490ef2eb5c2b85
Pretender8 <34997550+Pretender8@users.noreply.github.com>
2024-08-20 00:31:18 -0500

    broken code fixed

--- gmtk2024/scenes/title.gd
+++ gmtk2024/scenes/title.gd
@@ -4,14 +4,9 @@ extends Control
 #play button
 func _on_play_pressed() -> void:
 	pass # Replace with function body.
-
 #settings
-
-
-
-
-
-
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
 #credits
-
-#quit
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
